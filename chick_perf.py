91194e56b768169109ae5f8d8f4f4826be2bc3c1
knall0 <32388161+knall0@users.noreply.github.com>
2020-07-13 16:16:19 +0200

    Create chick_perf.py

---
+++ chick_perf.py
@@ -0,0 +1,107 @@
+from telegram.ext import Updater, CommandHandler
+
+
+class Chicken:
+    performance_template = \
+        "{id}'s Performance sind insgesamt {output} Eier mit einer aktuellen Streak von {streak} Eiern hintereinander."
+
+    def __init__(self, name, initial_streak=0, initial_output=0):
+        self.name = name
+        self.streak = initial_streak
+        self.output = initial_output
+
+    def performance_day(self):
+        self.streak += 1
+        self.output += 1
+
+    def lay_day(self):
+        self.streak = 0
+
+    def show_performance(self):
+        return self.performance_template.format(id=self.name,
+                                                output=self.output,
+                                                streak=self.streak)
+
+
+bogdan = Chicken('Bogdan', 10, 15)
+flash = Chicken('Flash', 15, 20)
+tanja = Chicken('Tanja', 20, 25)
+
+def start(update, context):
+    start_text = """
+    Hi!
+    Willkommen beim Performance-Hühnerbot:
+    
+    - Tippe /bogi_performed wenn bogdan heute performed hat
+    - Tippe /bogi_chillt wenn bogdan heute kein Ei gelegt hat
+    
+    - Tippe /flash_performed wenn flash heute performed hat
+    - Tippe /flash_chillt wenn flash heute kein Ei gelegt hat
+    
+    - Tippe /tanja_performed wenn tanja heute performed hat
+    - Tippe /tanja_chillt wenn tanja heute kein Ei gelegt hat
+    
+    - Tippe /status für die aktuellen Daten
+    """
+    update.message.reply_text(start_text)
+
+                              
+def bogdan_performance_day(update, context):
+    bogdan.performance_day()
+    update.message.reply_text(bogdan.show_performance())
+
+
+def bogdan_lay_day(update, context):
+    bogdan.lay_day()
+    update.message.reply_text(bogdan.show_performance())
+    
+    
+def flash_performance_day(update, context):
+    flash.performance_day()
+    update.message.reply_text(flash.show_performance())
+
+
+def flash_lay_day(update, context):
+    flash.lay_day()
+    update.message.reply_text(flash.show_performance())
+
+    
+def tanja_performance_day(update, context):
+    tanja.performance_day()
+    update.message.reply_text(tanja.show_performance())
+
+
+def tanja_lay_day(update, context):
+    tanja.lay_day()
+    update.message.reply_text(tanja.show_performance())
+    
+def status(update, context):
+    update.message.reply_text(bogdan.show_performance())
+    update.message.reply_text(flash.show_performance())
+    update.message.reply_text(tanja.show_performance())
+
+    
+    
+def main():
+    updater = Updater(token="1375900918:AAH-O1yud2bT0fXHGUXAi0txo-sTfW932xo", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("bogi_performed", bogdan_performance_day))
+    dp.add_handler(CommandHandler("bogi_chillt", bogdan_lay_day))
+    
+    dp.add_handler(CommandHandler("bogi_performed", bogdan_performance_day))
+    dp.add_handler(CommandHandler("flash_chillt", flash_lay_day))
+    
+    dp.add_handler(CommandHandler("flash_performed", flash_performance_day))
+    dp.add_handler(CommandHandler("tanja_chillt", tanja_lay_day))
+    dp.add_handler(CommandHandler("tanja_performed", tanja_performance_day))
+    
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("status", status))
+
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
